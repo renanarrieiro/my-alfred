7a48a03f071db293bf58a37c02d96d0b34892e8f
Renan Proferis <renan.proferis@sap.com>
2025-06-05 17:28:44 -0300

    wake alfred

---
+++ data/dialog_data/wake_key_words.py
@@ -0,0 +1,7 @@
+wake_key_words = [
+    "alfred",
+    "alfre",
+    "alfr",
+    "i am batman",
+    "i'm batman"
+]
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from functions.welcome import greeting
 from head.ear import listen
 from head.brain import think
+from data.dialog_data.wake_key_words import wake_key_words
 
 if __name__ == "__main__":
     lisining = False
@@ -10,9 +11,10 @@ if __name__ == "__main__":
         text_spoke = 'a'
         text_spoke = (listen() or "").lower()
 
-        if text_spoke == "alfred":
+        if text_spoke.replace(" ", "") in wake_key_words:
             greeting()
             lisining = True
+            
         if lisining == True:
             lisining = False
             print(think())
